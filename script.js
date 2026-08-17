eda514f37016dcfea30e4cd8d3ee016658e2b5a0
Mattias Bergstedt <mattiasbergstedt04@gmail.com>
2026-08-17 17:21:15 +0200

    Create function operate

--- script.js
+++ script.js
@@ -26,4 +26,19 @@ let secondOperand;
 let operator;
 
 // func operate takes operator and two operands and calls one of the above func with the operands
-
+function operate(firstOperand, operator, secondOperand) {
+    switch(operator) {
+        case "+":
+            return add(firstOperand, secondOperand);
+            break;
+        case "-":
+            return subtract(firstOperand, secondOperand);
+            break;
+        case "*":
+            return multiply(firstOperand, secondOperand);
+            break;
+        case "/":
+            return divide(firstOperand, secondOperand);
+            break;
+    }
+}
