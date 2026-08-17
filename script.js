6dd9b9fcf6365ada82459dbd87551a3fc4ada5a7
Mattias Bergstedt <mattiasbergstedt04@gmail.com>
2026-08-17 17:14:01 +0200

    Add functions for basic math operators

---
+++ script.js
@@ -0,0 +1,24 @@
+// function for add
+function add(firstOperand, secondOperand) {
+    return firstOperand + secondOperand;
+}
+
+// function for subtract
+function subtract(firstOperand, secondOperand) {
+    return firstOperand - secondOperand;
+}
+
+// function for multiply
+function multiply(firstOperand, secondOperand) {
+    return firstOperand * secondOperand;
+}
+
+// function for divide
+function divide(firstOperand, secondOperand) {
+    return firstOperand / secondOperand;
+}
+
+// three variables, firstOperand, operator, secondOperand
+
+// func operate takes operator and two operands and calls one of the above func with the operands
+
